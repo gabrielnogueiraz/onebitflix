f9b81e240246bdfcd8e72fb4961a05e4bc7f8102
Gabriel <108474013+gabrielnogueiraz@users.noreply.github.com>
2023-10-10 11:02:49 -0300

    obtaining courses from a category

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,5 +4,6 @@ import { categoriesControler } from "./controlers/categoriesControler";
 const router = express.Router();
 
 router.get("/categories", categoriesControler.index);
+router.get("/categories/:id", categoriesControler.show);
 
 export { router };
