230ce4dd74f69f31e71a511eb4834b532754713a
Gabriel <108474013+gabrielnogueiraz@users.noreply.github.com>
2023-10-16 11:25:16 -0300

    implementation of the category protection and video streaming system using Json Web Token

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,20 +3,22 @@ import { categoriesControler } from "./controlers/categoriesControler";
 import { coursesController } from "./controlers/coursesControler";
 import { episodesController } from "./controlers/episodesControler";
 import { authControler } from "./controlers/authControler";
+import { ensureAuth, ensureAuthViaQuery } from "./middlewares/auth";
 
 const router = express.Router();
 
 router.post("/auth/register", authControler.register);
 router.post("/auth/login", authControler.login);
 
-router.get("/categories", categoriesControler.index);
-router.get("/categories/:id", categoriesControler.show);
+router.get("/categories", ensureAuth, categoriesControler.index);
+router.get("/categories/:id", ensureAuth, categoriesControler.show);
 
-router.get("/courses/featured", coursesController.featured);
+router.get("/courses/featured", ensureAuth, coursesController.featured);
 router.get("/courses/newest", coursesController.newest);
-router.get("/courses/search", coursesController.search);
-router.get("/courses/:id", coursesController.show);
+router.get("/courses/search", ensureAuth, coursesController.search);
+router.get("/courses/:id", ensureAuth, coursesController.show);
 
-router.get("/episodes/stream", episodesController.stream);
+router.get("/episodes/stream", ensureAuthViaQuery,episodesController.stream);
 
 export { router };
+
