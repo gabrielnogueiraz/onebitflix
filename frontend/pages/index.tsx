3dac13b271c23cbe6861559ecfa5532970fecd49
Gabriel <108474013+gabrielnogueiraz@users.noreply.github.com>
2023-10-23 14:05:25 -0300

    creation of color variables and optimization of the non-auth home page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,17 @@
 import Head from "next/head";
-import styles from '../styles/HomeNoAuth.module.scss'
+import styles from "../styles/HomeNoAuth.module.scss";
 
 const HomeNoAuth = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>OneBitFlix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta
+          name="description"
+          content="Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil!"
+        />
       </Head>
       <main></main>
     </>
